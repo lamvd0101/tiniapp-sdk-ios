6bb679fb161fea95cfa3b0dbcc5f1c884701fbf3
Liam Vuong <lamvd0101@gmail.com>
2022-09-20 19:11:02 +0700

    Create xcframework

---
+++ ios/build.sh
@@ -0,0 +1,32 @@
+FRAMEWORK_NAME=TiniAppSDK
+OUTPUT_DIR=output
+
+rm -rf build
+rm -rf ${OUTPUT_DIR}
+
+SIMULATOR_ARCHIVE_PATH=./build/${FRAMEWORK_NAME}-iphonesimulator.xcarchive
+DEVICE_ARCHIVE_PATH=./build/${FRAMEWORK_NAME}-iphoneos.xcarchive
+
+# Simulator xcarchieve
+xcodebuild archive \
+  -workspace ${FRAMEWORK_NAME}.xcworkspace \
+  -scheme ${FRAMEWORK_NAME} \
+  -archivePath ${SIMULATOR_ARCHIVE_PATH} \
+  -configuration Release \
+  -sdk iphonesimulator \
+  SKIP_INSTALL=NO
+
+# Device xcarchieve
+xcodebuild archive \
+  -workspace ${FRAMEWORK_NAME}.xcworkspace \
+  -scheme ${FRAMEWORK_NAME} \
+  -archivePath ${DEVICE_ARCHIVE_PATH} \
+  -sdk iphoneos \
+  -configuration Release \
+  SKIP_INSTALL=NO
+
+# Create xcframwork combine of all frameworks
+xcodebuild -create-xcframework \
+  -framework ${SIMULATOR_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+  -framework ${DEVICE_ARCHIVE_PATH}/Products/Library/Frameworks/${FRAMEWORK_NAME}.framework \
+  -output ${OUTPUT_DIR}/${FRAMEWORK_NAME}.xcframework
