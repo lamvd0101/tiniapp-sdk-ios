f038a5d2e9523bbed3696b5c3e130420bb91bcdf
Han Han <han.han@tiki.vn>
2022-11-02 11:39:25 +0700

    bump v1.7.2

--- Frameworks/React.xcframework/ios-arm64/React.framework/Headers/RCTAlertManager.h
+++ Frameworks/React.xcframework/ios-arm64/React.framework/Headers/RCTAlertManager.h
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
@@ -16,7 +16,6 @@ typedef NS_ENUM(NSInteger, RCTAlertViewStyle) {
   RCTAlertViewStylePlainTextInput,
   RCTAlertViewStyleLoginAndPasswordInput
 };
-
 
 @interface RCTAlertManager : NSObject <RCTBridgeModule, RCTInvalidating>
 
--- Frameworks/React.xcframework/ios-arm64_x86_64-simulator/React.framework/Headers/RCTAppState.h
+++ Frameworks/React.xcframework/ios-arm64_x86_64-simulator/React.framework/Headers/RCTAppState.h
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
@@ -7,6 +7,6 @@
 
 #import <React/RCTEventEmitter.h>
 
-@interface RCTAppState : RCTEventEmitter
+@interface RCTAppState : RCTEventEmitter <RCTInvalidating>
 
 @end
